11f0837ea3f7acc3d1ddbdd043b4eb09dace16a9
Gladyshev Ilya <ilya30060@gmail.com>
2026-09-18 01:12:06 +0700

    Initial Git Folder Sync13

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,10 @@
 fn main() {
-    tauri_build::build()
+    println!("cargo:rerun-if-changed=permissions/android-saf");
+
+    let attributes = tauri_build::Attributes::new().plugin(
+        "android-saf",
+        tauri_build::InlinedPlugin::new(),
+    );
+
+    tauri_build::try_build(attributes).expect("failed to run tauri-build");
 }
